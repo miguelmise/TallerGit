7c29e11d3c16126e0d608aaadd735ffb6d1024b9
Miguel Mise <mimise@espol.edu.ec>
2021-10-20 17:20:55 -0500

    Agregando codigo base

---
+++ Taller01-Snake/src/lab1/Main.java
@@ -0,0 +1,16 @@
+package lab1;
+
+
+
+/**
+ * This class creates an AWT window which will contain the game.
+ */
+public class Main {
+	public static void main(final String[] args) {
+		
+		// Create a new frame (a window)
+		GUIView guiview = new GUIView();
+		guiview.setTitle("Taller Observer");
+
+	}
+}
