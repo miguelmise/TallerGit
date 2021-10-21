cbdf8bac7ebfec8f42481cff9409336555a064f1
sebyaguana <gyaguana@espol.edu.ec>
2021-10-20 19:28:39 -0500

    Participante-uno

--- Snake/src/lab1/GUIView.java
+++ Snake/src/lab1/GUIView.java
@@ -57,7 +57,7 @@ public class GUIView extends JPanel {
 
 		// Make a new panel containing the GUI
 		this.guiPanel = new JPanel();
-		this.startGameButton.setText("Let's Go");
+		this.startGameButton.setText("Let's Gooo");
 
 		// Set the background on that panel
 		this.guiPanel.setBackground(Color.lightGray);
