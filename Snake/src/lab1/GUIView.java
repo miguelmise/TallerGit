9166eedcef419b293875e1152824a6bf0c906e27
sebyaguana <gyaguana@espol.edu.ec>
2021-10-21 19:16:44 -0500

    Integrante 3 - resolucion de problemas

--- Snake/src/lab1/GUIView.java
+++ Snake/src/lab1/GUIView.java
@@ -57,7 +57,7 @@ public class GUIView extends JPanel {
 
 		// Make a new panel containing the GUI
 		this.guiPanel = new JPanel();
-		this.startGameButton.setText("Let's Gooo");
+		this.startGameButton.setText("Bienvenido al juego");
 		// Set the background on that panel
 		this.guiPanel.setBackground(Color.lightGray);
 
